e607ccd7714688f13dd1ab93409d0ea10a22f144
unknown <kaden.zipfel@hotmail.com>
2018-06-20 16:49:10 -0700

    Setup edit dispensary view and logic

--- routes/dispensaries.js
+++ routes/dispensaries.js
@@ -48,12 +48,30 @@ router.get('/:id', (req, res) => {
 
 // Edit dispensary page
 router.get('/:id/edit', (req, res) => {
-  res.render('dispensaries/edit');
+  Dispensary.findById(req.params.id, (err, dispensary) => {
+    if(err) {
+      console.log(err);
+    } else {
+      res.render('dispensaries/edit', {dispensary: dispensary});
+    }
+  });
 });
 
 // Edit dispensary logic
 router.put('/:id', (req, res) => {
-  
+  Dispensary.findByIdAndUpdate(req.params.id, {
+    name: req.body.name,
+    location: req.body.location,
+    description: req.body.description,
+    image: req.body.image
+  }, (err, dispensary) => {
+    if(err){
+      console.log(err);
+      res.redirect('/dispensaries');
+    } else {
+      res.redirect('/dispensaries/' + req.params.id);
+    }
+  });
 });
 
 // Delete dispensary logic
